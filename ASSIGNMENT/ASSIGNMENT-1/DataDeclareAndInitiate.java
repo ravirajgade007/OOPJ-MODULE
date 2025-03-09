5316577ee848a3d077c55995691fd60a84d1b86d
Raviraj Gade <130790538+ravirajgade007@users.noreply.github.com>
2025-03-09 20:43:25 +0530

    Add files via upload

---
+++ ASSIGNMENT/ASSIGNMENT-1/DataDeclareAndInitiate.java
@@ -0,0 +1,24 @@
+class DataDeclareAndInitiate
+{
+  public static void main(String args[])
+   {
+      byte bt = 120;
+      short s = 5000;
+      int i = 547872125;
+      long l = 54879874125457845L;
+      float f = 45.12F;
+      double d = 787.4587D;
+      char c = 'R';
+      boolean b = true;
+
+
+     System.out.println("Byte:" + bt);
+     System.out.println("Short:" + s);
+     System.out.println("Int:" + i);
+     System.out.println("Long:" + l);
+     System.out.println("Float:" + f);
+     System.out.println("Double:" + d);
+     System.out.println("Char:" + c);
+     System.out.println("Boolean:" + b); 
+   }
+}
